8a83b2d5f7f9c0686982124b950eea10b8607de7
Mitchell <meechllada@gmail.com>
2017-11-16 14:43:22 -0800

    create routes to beer details

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,9 +1,13 @@
 class BeersController < ApplicationController
   def index
-    
+    @style = Style.find(params[:style_id])
+    @beer = @style.beers.all
   end
 
   def show
+    @style = Style.find(params[:style_id])
+    @beer = @style.beers.find(params[:id])
+    render :show
   end
 
   def new
@@ -21,5 +25,6 @@ class BeersController < ApplicationController
   private
   def beer_params
     params.require(:beer).permit(:name, :alcohol_content, :ibu, :blg, :hops, :yeast, :malt, :bio)
+  end
 
 end
